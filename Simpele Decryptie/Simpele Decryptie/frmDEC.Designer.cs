f3560eb4ee4aacd67cb611336184cc2a5c560376
P-Vries <43247243+P-Vries@users.noreply.github.com>
2018-09-19 21:15:08 +0200

    Add files via upload

---
+++ Simpele Decryptie/Simpele Decryptie/frmDEC.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Simpele_Decryptie
+{
+    partial class frmDEC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbInvoer = new System.Windows.Forms.RichTextBox();
+            this.txbUitvoer = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnOpenen = new System.Windows.Forms.Button();
+            this.btnUitOpslaan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Teskt: invoer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tekst Uivoer:";
+            // 
+            // txbInvoer
+            // 
+            this.txbInvoer.Location = new System.Drawing.Point(12, 25);
+            this.txbInvoer.Name = "txbInvoer";
+            this.txbInvoer.ReadOnly = true;
+            this.txbInvoer.Size = new System.Drawing.Size(776, 135);
+            this.txbInvoer.TabIndex = 2;
+            this.txbInvoer.Text = "";
+            // 
+            // txbUitvoer
+            // 
+            this.txbUitvoer.Location = new System.Drawing.Point(12, 179);
+            this.txbUitvoer.Name = "txbUitvoer";
+            this.txbUitvoer.Size = new System.Drawing.Size(776, 230);
+            this.txbUitvoer.TabIndex = 3;
+            this.txbUitvoer.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnOpenen
+            // 
+            this.btnOpenen.Location = new System.Drawing.Point(682, 415);
+            this.btnOpenen.Name = "btnOpenen";
+            this.btnOpenen.Size = new System.Drawing.Size(106, 23);
+            this.btnOpenen.TabIndex = 5;
+            this.btnOpenen.Text = "Bestand Openen";
+            this.btnOpenen.UseVisualStyleBackColor = true;
+            this.btnOpenen.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnUitOpslaan
+            // 
+            this.btnUitOpslaan.Location = new System.Drawing.Point(351, 415);
+            this.btnUitOpslaan.Name = "btnUitOpslaan";
+            this.btnUitOpslaan.Size = new System.Drawing.Size(97, 23);
+            this.btnUitOpslaan.TabIndex = 6;
+            this.btnUitOpslaan.Text = "Uitvoer Opslaan";
+            this.btnUitOpslaan.UseVisualStyleBackColor = true;
+            this.btnUitOpslaan.Click += new System.EventHandler(this.btnUitOpslaan_Click);
+            // 
+            // frmDEC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUitOpslaan);
+            this.Controls.Add(this.btnOpenen);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txbUitvoer);
+            this.Controls.Add(this.txbInvoer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDEC";
+            this.Text = "Simpele Versleuteling (DEC)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txbInvoer;
+        private System.Windows.Forms.RichTextBox txbUitvoer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnOpenen;
+        private System.Windows.Forms.Button btnUitOpslaan;
+    }
+}
+
---
+++ Simpele Decryptie/Simpele Decryptie/frmDEC.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Simpele_Decryptie
+{
+    public partial class frmDEC : Form
+    {
+        //
+        //Versie 5
+        //
+
+        //
+        //Vars
+        //
+        private string Out;//Deze wordt later gebruikt voor de Decryptie
+
+        public frmDEC()
+        {
+            InitializeComponent();
+        }
+
+        //
+        //Methodes
+        //
+        private void Decryptie()// Decryptie Methode
+        {
+            
+            byte randomInput = byte.Parse(txbInvoer.Text.Substring(0, 1));// Hier kijken we naar het eerste cijfer wat gebruikt word om te ontcijfewren later
+            txbUitvoer.Text = "";//Maakt de TekstBox Leeg
+            foreach (char c in txbInvoer.Text)// Loop voor elk teken in de tekstbox
+            {
+                char decrypted = (char)(c - randomInput);// maakt een nieuw teken door het eerste nummer aftetrekken aan de ascii-code van een teken
+                Out += decrypted.ToString();//voegt het aangepaste teken aan de output van de methode
+            }
+            Out = Out.Substring(1);//Hier verwijderen we het eerste nummer waarmee we de waarde hadden vertaald
+            txbUitvoer.Text = Out;//Hier voegen we het toe aan het scherm.
+            Out = "";// Hier maken we deze variabel leeg zodat we hem opnieuw kunnen gebruiken zonder problemen
+        }
+
+        private void Openen()//Openen Methode
+        {
+            Stream dezeStream; //We maken een stream variabel aan zodat we van een File kunnen lezen.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();//We maken een Nieuwe FileDialoog aan en noemen het " fileDialog1"
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)// We kijken of de gebruiker op de OK knopo klikt in het dialoog met de computer
+            {
+                if ((dezeStream = openFileDialog1.OpenFile())!= null)//We maken de Stream gelijk aan de openFileDialog en kijken of deze niet leeg is. 
+                {
+                    string fileName = openFileDialog1.FileName;//De bestands locatie slaan we op in de string fileName
+                    string fileTekst = File.ReadAllText(fileName);//Alle Tekst in het bestand word in de fileTekst string opgeslagen
+                    txbInvoer.Text = fileTekst; // De Tekstbox wordt gevuld met de string 
+                }
+            }
+        }
+        private void Opslaan() //Opslaan Methode
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();// Laat een save file dialoog zien
+            saveFileDialog1.Filter = "Text File | *.txt";//We laten ze kiezen uit meerdere bestanden of in dit geval alleen een txt File
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)// als er Ja of Ok wordt geklikt wordt dit uitgevoerd
+            {
+                try
+                {
+                    using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))//We gebruiken een stream om een nieuwe File te maken
+                    using (StreamWriter sw = new StreamWriter(s))// We gebruiken dit om naar de net gemaakte File te schrijven.
+                    {
+                        sw.Write(txbUitvoer.Text);// De tekst wordt doorgegeven aan de SteamWriter
+                    }
+                }// We maken deze try-Catch omdat er mischien al een file bestaat
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }//Laat de error Message zien op het scherrm
+
+            }
+
+        }
+
+        //
+        //Events
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Decryptie();//Verwijzeing naar de Decryptie methode
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Openen();//Verwijzing naar de Opnenen methhode
+        }
+
+        private void btnUitOpslaan_Click(object sender, EventArgs e)
+        {
+            Opslaan();//Verwijzing naar de Opslag methode
+        }
+    }
+}
