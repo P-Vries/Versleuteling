1dffbdc58198c2698fbe1f6e624cf86e0c8da943
P-Vries <43247243+P-Vries@users.noreply.github.com>
2018-09-19 21:16:11 +0200

    Add files via upload

---
+++ Simpele Encryptie/Versleutelen/frmSimpelEncryptie.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Versleutelen
+{
+    partial class frmSimpelEncryptie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbSimpelTekst = new System.Windows.Forms.RichTextBox();
+            this.txbEncTekst = new System.Windows.Forms.RichTextBox();
+            this.btnEncryptie = new System.Windows.Forms.Button();
+            this.btnSaveOutput = new System.Windows.Forms.Button();
+            this.btnOpenen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Invoer Text:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 251);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uitvoer Tekst:";
+            // 
+            // txbSimpelTekst
+            // 
+            this.txbSimpelTekst.Location = new System.Drawing.Point(12, 25);
+            this.txbSimpelTekst.Name = "txbSimpelTekst";
+            this.txbSimpelTekst.Size = new System.Drawing.Size(776, 223);
+            this.txbSimpelTekst.TabIndex = 2;
+            this.txbSimpelTekst.Text = "";
+            // 
+            // txbEncTekst
+            // 
+            this.txbEncTekst.Location = new System.Drawing.Point(12, 267);
+            this.txbEncTekst.Name = "txbEncTekst";
+            this.txbEncTekst.ReadOnly = true;
+            this.txbEncTekst.Size = new System.Drawing.Size(776, 142);
+            this.txbEncTekst.TabIndex = 3;
+            this.txbEncTekst.Text = "";
+            // 
+            // btnEncryptie
+            // 
+            this.btnEncryptie.Location = new System.Drawing.Point(12, 415);
+            this.btnEncryptie.Name = "btnEncryptie";
+            this.btnEncryptie.Size = new System.Drawing.Size(75, 23);
+            this.btnEncryptie.TabIndex = 4;
+            this.btnEncryptie.Text = "Encryptie";
+            this.btnEncryptie.UseVisualStyleBackColor = true;
+            this.btnEncryptie.Click += new System.EventHandler(this.btnEncryptie_Click);
+            // 
+            // btnSaveOutput
+            // 
+            this.btnSaveOutput.Location = new System.Drawing.Point(688, 415);
+            this.btnSaveOutput.Name = "btnSaveOutput";
+            this.btnSaveOutput.Size = new System.Drawing.Size(100, 23);
+            this.btnSaveOutput.TabIndex = 6;
+            this.btnSaveOutput.Text = "Uitvoer Opslaan";
+            this.btnSaveOutput.UseVisualStyleBackColor = true;
+            this.btnSaveOutput.Click += new System.EventHandler(this.btnSaveOutput_Click);
+            // 
+            // btnOpenen
+            // 
+            this.btnOpenen.Location = new System.Drawing.Point(351, 415);
+            this.btnOpenen.Name = "btnOpenen";
+            this.btnOpenen.Size = new System.Drawing.Size(103, 23);
+            this.btnOpenen.TabIndex = 7;
+            this.btnOpenen.Text = "Bestand Openen";
+            this.btnOpenen.UseVisualStyleBackColor = true;
+            this.btnOpenen.Click += new System.EventHandler(this.btnOpslaan_Click);
+            // 
+            // frmSimpelEncryptie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpenen);
+            this.Controls.Add(this.btnSaveOutput);
+            this.Controls.Add(this.btnEncryptie);
+            this.Controls.Add(this.txbEncTekst);
+            this.Controls.Add(this.txbSimpelTekst);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSimpelEncryptie";
+            this.Text = "Simpele Versleuteling (ENC)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txbSimpelTekst;
+        private System.Windows.Forms.RichTextBox txbEncTekst;
+        private System.Windows.Forms.Button btnEncryptie;
+        private System.Windows.Forms.Button btnSaveOutput;
+        private System.Windows.Forms.Button btnOpenen;
+    }
+}
+
---
+++ Simpele Encryptie/Versleutelen/frmSimpelEncryptie.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Versleutelen
+{
+    public partial class frmSimpelEncryptie : Form
+    {
+
+        //
+        // Versie 5
+        //
+
+        public frmSimpelEncryptie()
+        {
+            InitializeComponent();
+        }
+        //
+        //Methodes
+        //
+        private void Encryptie()// Encryptie Methode
+        {
+            Random rnd = new Random();//Maak een nieuwe random generator
+            byte random = (byte)rnd.Next(1, 10);//genereer een nummer van 1 tot 10
+            txbEncTekst.Text = random.ToString();//Maakt de TekstBox Leeg
+            foreach (char c in txbSimpelTekst.Text)// Loop voor elk teken in de tekstbox
+            {
+                char encrypted = (char)(c + random);// maakt een nieuw teken door een Willekeurig nummer van 1 tot 10 toetevoegen aan de ascii-code van een teken
+                txbEncTekst.Text += encrypted.ToString();//voegt het aangepaste teken aan de output van de toepassing
+            }
+        }
+        private void Opslaan() //Opslaan Methode
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();// Laat een save file dialoog zien
+            saveFileDialog1.Filter = "Text File | *.txt";//We laten ze kiezen uit meerdere bestanden of in dit geval alleen een txt File
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)// als er Ja of Ok wordt geklikt wordt dit uitgevoerd
+            {
+                try
+                {
+                    using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))//We gebruiken een stream om een nieuwe File te maken
+                    using (StreamWriter sw = new StreamWriter(s))// We gebruiken dit om naar de net gemaakte File te schrijven.
+                    {
+                        sw.Write(txbEncTekst.Text);// De tekst wordt doorgegeven aan de SteamWriter
+                    }
+                }// We maken deze try-Catch omdat er mischien al een file bestaat
+                catch(Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }//Laat de error Message zien op het scherrm
+                
+            }
+            
+        }
+        private void Openen()//Openen Methode
+        {
+            Stream dezeStream; //We maken een stream variabel aan zodat we van een File kunnen lezen.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();//We maken een Nieuwe FileDialoog aan en noemen het " fileDialog1"
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)// We kijken of de gebruiker op de OK knopo klikt in het dialoog met de computer
+            {
+                if ((dezeStream = openFileDialog1.OpenFile()) != null)//We maken de Stream gelijk aan de openFileDialog en kijken of deze niet leeg is. 
+                {
+                    string fileName = openFileDialog1.FileName;//De bestands locatie slaan we op in de string fileName
+                    string fileTekst = File.ReadAllText(fileName);//Alle Tekst in het bestand word in de fileTekst string opgeslagen
+                    txbSimpelTekst.Text = fileTekst; // De Tekstbox wordt gevuld met de string 
+                }
+            }
+        }
+
+        //
+        //Events
+        //
+        private void btnEncryptie_Click(object sender, EventArgs e)// Encryptie
+        {
+            Encryptie();//Verwijzing naar de Encryptie Methode
+        }
+
+        private void btnSaveOutput_Click(object sender, EventArgs e)//Opslaan
+        {
+            Opslaan();//Verwijzing naar de Opslaan Methode
+        }
+
+        private void btnOpslaan_Click(object sender, EventArgs e)
+        {
+            Openen();//Verwijzing naar de Openen Methode
+        }
+    }
+}
